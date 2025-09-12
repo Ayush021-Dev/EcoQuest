681651854c3ffeeec72b0ad1ee00edca1d527b90
Sourabh-Web21 <artistbros007@gmail.com>
2025-09-12 18:17:14 +0530

    hua commit?

--- scripts/Char_scripts/player.gd
+++ scripts/Char_scripts/player.gd
@@ -4,14 +4,20 @@ extends CharacterBody2D
 var last_direction := "down"
 var half_extent = Vector2(16, 16)
 var center_offset = Vector2(0, 15)
-
 @onready var walking_sound = $WalkingSound
+
+# Path to Roads TileMap node (can be changed if some scenes have different hierarchy)
+@export var roads_node_name := "Roads"
 
 func can_move_to(dir: Vector2, delta: float) -> bool:
 	if dir == Vector2.ZERO:
 		return false
-	var tilemap = get_parent().get_node("Roads")
+	var tilemap = get_parent().get_node_or_null(roads_node_name)
+	if tilemap == null:
+		push_warning("TileMap node '%s' not found in parent scene." % roads_node_name)
+		return false
 	var next_pos = global_position + dir * speed * delta
+
 	for offset in [
 		Vector2(0, 0),
 		Vector2(-half_extent.x, -half_extent.y),
@@ -23,9 +29,9 @@ func can_move_to(dir: Vector2, delta: float) -> bool:
 		var local_pos = tilemap.to_local(check_pos)
 		var cell = tilemap.local_to_map(local_pos)
 		var cell_data = tilemap.get_cell_tile_data(cell)
-		if not (cell_data and (cell_data.get_terrain()== 0 or cell_data.get_terrain()== 1)):
+		# Only allow terrain 0 or 1
+		if not (cell_data and (cell_data.get_terrain() == 0 or cell_data.get_terrain() == 1)):
 			return false
-
 	return true
 
 func _physics_process(delta):
@@ -38,10 +44,9 @@ func _physics_process(delta):
 		input_dir.y += 1
 	if Input.is_action_pressed("ui_up"):
 		input_dir.y -= 1
-
 	input_dir = input_dir.normalized()
-	var move_dir = Vector2.ZERO
 
+	var move_dir = Vector2.ZERO
 	if can_move_to(input_dir, delta):
 		move_dir = input_dir
 	else:
@@ -54,11 +59,9 @@ func _physics_process(delta):
 	if move_dir != Vector2.ZERO:
 		velocity = move_dir * speed
 		move_and_slide()
-
 		# Play walking sound if not playing
 		if not walking_sound.playing:
 			walking_sound.play()
-
 		# Animations
 		if move_dir.x > 0:
 			$AnimatedSprite2D.play("walk_right")
@@ -75,7 +78,6 @@ func _physics_process(delta):
 	else:
 		velocity = Vector2.ZERO
 		$AnimatedSprite2D.play("idle_" + last_direction)
-
 		# Stop walking sound when idle
 		if walking_sound.playing:
 			walking_sound.stop()
